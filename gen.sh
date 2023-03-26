3c30a4573f123552689a5bf53ef2eef737a6bb74
Teddy Li <teddyxlandlee@hotmail.com>
2023-03-26 09:33:04 +0800

    version.txt

--- gen.sh
+++ gen.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 cd $(dirname $0)
+
 if [ -e dist/sample.js ] ; then
  mv dist/sample.js dist/sample.js.old
 fi
+
+if [ -e version.txt ] ; then
+ rm version.txt
+fi
+date -Iseconds >> version.txt
+
 ./gendist.py gen.json dist/sample.js
